25e49330718520e331a4b63da73191b8535d26b3
Kinoken0413 <kengo.kinouchi@gmail.com>
2023-12-20 20:56:46 +0900

    商品出品機能の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { Faker::Commerce.product_name[0...40] }
+    item_explanation {Faker::Lorem.sentence[0...1000]}
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    sales_status_id { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_status_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    scheduled_delivery_id { Faker::Number.between(from: 2, to: 3) }
+    item_price { Faker::Number.between(from: 300, to: 9999999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,88 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品できる場合' do
+      it '情報がすべて入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できない場合' do
+      it 'imageなしでは登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'item_nameが空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'item_nameが41文字以上では登録できない' do
+        @item.item_name = 'a' * 41
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item name is too long (maximum is 40 characters)')
+      end
+      it 'item_explanationが空では登録できない' do
+        @item.item_explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item explanation can't be blank")
+      end
+      it 'item_explanationが1000文字以上では登録できない' do
+        @item.item_explanation = 'a' * 1001
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item explanation is too long (maximum is 1000 characters)')
+      end
+      it 'category_idが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'sales_status_idが空では登録できない' do
+        @item.sales_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
+      end
+      it 'shipping_fee_status_idが空では登録できない' do
+        @item.shipping_fee_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
+      end
+      it 'prefecture_idが空では登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'scheduled_delivery_idが空では登録できない' do
+        @item.scheduled_delivery_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+      end
+      it 'item_priceが空では登録できない' do
+        @item.item_price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price can't be blank")
+      end
+      it 'item_priceが半角数字以外では登録できない' do
+        @item.item_price = '１２３４'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is not a number')
+      end
+      it 'item_priceが300より小さいと登録できない' do
+        @item.item_price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price must be greater than or equal to 300')
+      end
+      it 'item_priceが9,999,999より大きいと登録できない' do
+        @item.item_price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price must be less than or equal to 9999999')
+      end
+    end
+  end
 end
