576add3fb23a554855f0cb52ff4c5f5e62574806
Kinoken0413 <kengo.kinouchi@gmail.com>
2023-12-22 16:43:38 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only:[:new]
+  before_action :authenticate_user!, only:[:new, :edit]
+  before_action :set_item, only:[:show, :edit, :update]
+  before_action :check_owner, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -21,7 +23,19 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   private
@@ -38,6 +52,16 @@ class ItemsController < ApplicationController
       :scheduled_delivery_id,
       :item_price
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_owner
+    unless @item.user == current_user
+      redirect_to '/'
+    end
   end
 
 end
